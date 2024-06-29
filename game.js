bf643def9524224f20e3c917fcc7286db8992670
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-06-29 18:27:53 +0300

    generating audio of the random button

--- game.js
+++ game.js
@@ -3,6 +3,7 @@ let gamePattern = [];
 let userClickedPattern = []
 let started = false
 let level = 0
+let soundNames = ['red', 'blue', 'green' ,  'yellow'];
 
 
 document.addEventListener('keypress', () => {
@@ -11,18 +12,37 @@ document.addEventListener('keypress', () => {
         document.querySelector("#level-title").textContent = `Level ${level}`
         nextSequence();
         started = true
+    }
+   
+
+});
         function nextSequence(){
-            level++;
-            document.querySelector("#level-title").textContent = `Level ${level}`
-            started = true
+              level++;
+              document.querySelector("#level-title").textContent = `Level ${level}`
+            //started = true
+            generate();
+            //playAudio();
         }
+
+        //simon randomly will choose a button
         function generate(){
             
-            let random =  Math.floor((Math.random()*buttonColors.length));
-            let randomButton = buttonColors[random];
-            console.log(randomButton);
+           let random =  Math.floor((Math.random()*buttonColors.length));
+           let randomButton = buttonColors[random];
+           console.log(randomButton);
+           gamePattern.push(randomButton);
+           console.log(gamePattern);
+           let selectedSoundName = soundNames[random];
+           console.log(selectedSoundName);
+            let soundPath = `sounds/${selectedSoundName}.mp3`;
+            let audio = new Audio(soundPath);
+            audio.play();
     }
-    generate()
-}
-})
+    
+        
+    
+
+    
+
+
 
