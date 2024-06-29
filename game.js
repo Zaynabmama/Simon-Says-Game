22c95f7f049f63e8109b7357ab544129a2cf63ed
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-06-29 17:57:57 +0300

    adding some functions

--- game.js
+++ game.js
@@ -4,12 +4,25 @@ let userClickedPattern = []
 let started = false
 let level = 0
 
+
 document.addEventListener('keypress', () => {
 
     if (!started) {
         document.querySelector("#level-title").textContent = `Level ${level}`
         nextSequence();
         started = true
+        function nextSequence(){
+            level++;
+            document.querySelector("#level-title").textContent = `Level ${level}`
+            started = true
+        }
+        function generate(){
+            
+            let random =  Math.floor((Math.random()*buttonColors.length));
+            let randomButton = buttonColors[random];
+            console.log(randomButton);
     }
+    generate()
+}
 })
 
